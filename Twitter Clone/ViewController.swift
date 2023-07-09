fe532de2efba3fa0444dcaeb90d123fafbfe6502
Colokol <Colokol1@gmail.com>
2023-07-09 20:23:20 +0300

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Uladzislau Yatskevich on 9.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
